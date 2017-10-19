39b346d267f5827cc4dbc8743733424f3f753194
Daniel Morandini <danielmorandini@me.com>
2017-10-19 18:04:32 +0200

    Update documentation

--- booster/booster.go
+++ booster/booster.go
@@ -1,9 +1,4 @@
 // Package booster provides the core functionality of the whole booster concept.
-// It is responsible for maintainig the `helper` devices group, collecting parallelizable
+// It is responsible for maintainig the connections with the `forwarder` devices, collecting parallelizable
 // requests and giving the appropriate work load to each device.
-//
-// Booster is not responsible for request/server checking, i.e. each request that it
-// is going to process always comes togheter with the parallelisation procedure that
-// booster should exploit, for example "take this request and multiplex it using"
-// HTTP Bytes range headers.
 package booster
