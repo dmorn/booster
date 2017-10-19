7077956ba57a213deb173e1b3ae49984844c2f76
Daniel Morandini <danielmorandini@me.com>
2017-10-19 18:01:08 +0200

    Clean codebase

--- checker/checker.go
+++
@@ -1,3 +0,0 @@
-// Package checker provides a mechanism for enstablishing if and how an HTTP request
-// can be multiplexed. Once this information in retriven, the request can be boosted.
-package checker
