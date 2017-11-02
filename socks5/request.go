420b4e1e97334bd7f73613b5be8bacf3eb9fa34e
Daniel Morandini <danielmorandini@me.com>
2017-11-02 20:12:38 +0100

    Add request/response

---
+++ socks5/request.go
@@ -0,0 +1,73 @@
+package socks5
+
+import "fmt"
+
+type Request struct {
+	Unmarshaler
+
+	Ver      uint8
+	Cmd      uint8
+	AddrType uint8
+	Addr     []uint8
+	DstPort  []uint8
+}
+
+// Unmarshal fill r with data contained in p.
+// expected input format:
+//
+// +----+-----+-------+------+----------+----------+
+// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
+// +----+-----+-------+------+----------+----------+
+// | 1  |  1  | X'00' |  1   | Variable |    2     |
+// +----+-----+-------+------+----------+----------+
+// numers represent field size
+//
+func (r *Request) Unmarshal(p []byte) error {
+	expl := 5 // expected p length
+	if len(p) < expl {
+		return fmt.Errorf("unexpected input length. found %v", len(p))
+	}
+	v := p[0]    // version
+	cmd := p[1]  // command
+	atyp := p[3] // address type
+
+	// destination address
+	var addr []byte
+	var addrl int // address length
+	asi := 4      // address start index
+
+	switch atyp {
+	case AddrTypeIPV4:
+		addrl = 4
+	case AddrTypeIPV6:
+		addrl = 16
+	case AddrTypeDomainName:
+		addrl = int(p[4])
+		asi = 5
+	}
+
+	expl = 6 + addrl
+	if len(p) < expl {
+		return fmt.Errorf("unexpected input length. found %v", len(p))
+	}
+
+	addr = make([]uint8, addrl)
+	copy(addr, p[asi:(asi+addrl)])
+	if len(addr) == 0 {
+		return fmt.Errorf("unable to parse destination address")
+	}
+
+	// destination port
+	dstPort := p[(asi + len(addr)):2]
+	if len(dstPort) == 0 {
+		return fmt.Errorf("unable to parse destination port")
+	}
+
+	r.Ver = v
+	r.Cmd = cmd
+	r.AddrType = atyp
+	r.Addr = addr
+	r.DstPort = dstPort
+
+	return nil
+}
---
+++ socks5/response.go
@@ -0,0 +1,39 @@
+package socks5
+
+type Response struct {
+	Marshaler
+
+	Ver      uint8
+	Rep      uint8 // reponse code
+	Rsv      uint8
+	AddrType uint8
+	BndAddr  []uint8
+	BndPort  []uint8
+}
+
+// Marshal converts r into []byte.
+// Output format:
+//
+// +----+-----+-------+------+----------+----------+
+// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+// +----+-----+-------+------+----------+----------+
+// | 1  |  1  | X'00' |  1   | Variable |    2     |
+// +----+-----+-------+------+----------+----------+
+// numers represent field size
+//
+func (r *Response) Marshal() ([]byte, error) {
+	addrl := len(r.BndAddr)
+	bas := 4         // BDN.ADDR start index
+	bae := 4 + addrl // BDN.ADDR end index
+	bufsize := 6 + addrl
+	buf := make([]uint8, bufsize)
+
+	buf[0] = r.Ver
+	buf[1] = r.Rep
+	buf[2] = FieldReserved
+	buf[3] = r.AddrType
+	copy(buf[bas:bas+bae], r.BndAddr)
+	copy(buf[bae:bae+2], r.BndPort)
+
+	return buf, nil
+}
