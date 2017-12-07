1750e8f06c45be576d565ca06d8329b57fa81d10
Daniel Morandini <danielmorandini@me.com>
2017-12-07 16:51:00 +0100

    cleanup

--- socks5/connect.go
+++ socks5/connect.go
@@ -10,14 +10,13 @@ import (
 // be a canonical address with host and port, according
 // to RFC 1928.
 func (s *Socks5) Connect(ctx context.Context, conn net.Conn, target string) (net.Conn, error) {
-
 	s.Printf("connect to %v", target)
 
 	// cap is just an estimation
 	buf := make([]byte, 0, 6+len(target))
 	buf = append(buf, socks5Version)
 
-	tconn, err := s.Dialer.DialContext(ctx, "tcp", target)
+	tconn, err := s.DialContext(ctx, "tcp", target)
 	if err != nil {
 		// TODO(daniel): Responde with proper code
 		buf = append(buf, socks5RespHostUnreachable, socks5FieldReserved)
