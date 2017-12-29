4acf21389013878bafebf39e9d330dcb46b48987
Daniel Morandini <danielmorandini@me.com>
2017-12-29 11:42:14 +0100

    docs

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,3 +1,5 @@
+// Package pubsub provides the core functionalities to handle
+// publication/subscription pipelines.
 package pubsub
 
 import (
@@ -7,27 +9,20 @@ import (
 	"time"
 )
 
+// PubSub wraps the core pubsub functionalities.
 type PubSub struct {
 	registry map[string]*medium
 }
 
+// New returns a new PubSub instance.
 func New() *PubSub {
 	return &PubSub{
 		registry: make(map[string]*medium),
 	}
 }
 
-func (ps *PubSub) medium(topic string) (*medium, error) {
-	hash := hash(topic)
-	m, ok := ps.registry[hash]
-	if !ok {
-		return nil, errors.New("pubsub: topic " + topic + " not found")
-	}
-
-	return m, nil
-}
-
-func (ps *PubSub) Links(topic string) ([]link, error) {
+// Links returns the active channels related to topic.
+func (ps *PubSub) Links(topic string) ([]chan interface{}, error) {
 	m, err := ps.medium(topic)
 	if err != nil {
 		return nil, err
@@ -36,19 +31,23 @@ func (ps *PubSub) Links(topic string) ([]link, error) {
 	return m.links, nil
 }
 
+// Sub makes a subscription to topic. Returns the channel where
+// the messages will be sent to, which should not be closed. If doing so,
+// the channel will be removed from the list of subscribed channels.
 func (ps *PubSub) Sub(topic string) chan interface{} {
 	hash := hash(topic)
 	m, err := ps.medium(topic)
 	if err != nil {
 		m = &medium{
-			done: make(chan struct{}),
-			id:   hash,
+			done:  make(chan struct{}),
+			id:    hash,
+			topic: topic,
 		}
 		ps.registry[hash] = m
 	}
 
 	if m.links == nil {
-		m.links = []link{}
+		m.links = []chan interface{}{}
 	}
 
 	l := make(chan interface{})
@@ -57,6 +56,9 @@ func (ps *PubSub) Sub(topic string) chan interface{} {
 	return l
 }
 
+// Unsub removes c from the list of subscribed channels of topic.
+// Returns an error if no such topic is present, or if the channel
+// is already no longer in the subscription list.
 func (ps *PubSub) Unsub(c chan interface{}, topic string) error {
 	m, err := ps.medium(topic)
 	if err != nil {
@@ -66,8 +68,8 @@ func (ps *PubSub) Unsub(c chan interface{}, topic string) error {
 	links := m.links
 	for i, l := range links {
 		if l == c {
-			close(m.links[i])
-			m.links = append(m.links[:i], m.links[i+1:]...)
+			close(m.links[i])                               // close the unsubscribed channel
+			m.links = append(m.links[:i], m.links[i+1:]...) // remove it from the list
 			return nil
 		}
 	}
@@ -75,6 +77,7 @@ func (ps *PubSub) Unsub(c chan interface{}, topic string) error {
 	return errors.New("pubsub: unsub error: unable to find channel")
 }
 
+// Close removes a topic and closes its related channels.
 func (ps *PubSub) Close(topic string) error {
 	m, err := ps.medium(topic)
 	if err != nil {
@@ -89,45 +92,58 @@ func (ps *PubSub) Close(topic string) error {
 	return nil
 }
 
+// Pub broadcasts the message to the listeners of topic.
+// Retuns an error if no such topic is present, unsubscribes
+// a channel if it is closed when sending to it. (i.e. causes a panic)
 func (ps *PubSub) Pub(message interface{}, topic string) error {
 	m, err := ps.medium(topic)
 	if err != nil {
 		return err
 	}
 
-	m.broadcast(message)
+	ps.broadcast(message, m)
 	return nil
 }
 
-type link chan interface{}
-
-type medium struct {
-	id    string
-	done  chan struct{}
-	links []link
-}
-
-func (m *medium) broadcast(data interface{}) {
+func (ps *PubSub) broadcast(message interface{}, medium *medium) {
 	send := func(c chan interface{}) {
 		defer func() {
 			if r := recover(); r != nil {
-				return
+				// remove the channel that caused the panic
+				ps.Unsub(c, medium.topic)
 			}
 		}()
 
 		select {
-		case c <- data:
+		case c <- message:
 		case <-time.After(time.Second * 5):
 		}
 	}
 
-	for _, l := range m.links {
+	for _, l := range medium.links {
 		go send(l)
 	}
 }
 
+func (ps *PubSub) medium(topic string) (*medium, error) {
+	hash := hash(topic)
+	m, ok := ps.registry[hash]
+	if !ok {
+		return nil, errors.New("pubsub: topic " + topic + " not found")
+	}
+
+	return m, nil
+}
+
+type medium struct {
+	id    string
+	topic string
+	done  chan struct{}
+	links []chan interface{}
+}
+
 func (m *medium) String() string {
-	return m.id
+	return "medium (" + m.id + "): topic: " + m.topic
 }
 
 func hash(images ...string) string {
