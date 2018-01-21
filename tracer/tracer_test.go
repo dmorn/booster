a6e60576dfc620d1570702c83d639e8a6d80d0e1
Daniel Morandini <danielmorandini@me.com>
2018-01-21 16:07:29 +0100

    :art:

--- tracer/tracer_test.go
+++ tracer/tracer_test.go
@@ -8,21 +8,6 @@ import (
 	"github.com/danielmorandini/booster-network/tracer"
 )
 
-func TestRun(t *testing.T) {
-	tr := tracer.NewDefault()
-	if tr.Status() != tracer.TrackerStatusRunning {
-		t.Fatalf("unexpected tracer status: found %v, expected %v", tr.Status(), tracer.TrackerStatusRunning)
-	}
-
-	if err := tr.Run(); err == nil {
-		t.Fatal("tracer should be running")
-	}
-
-	tr.Close()
-	if tr.Status() != tracer.TrackerStatusStopped {
-		t.Fatalf("unexpected tracer status: found %v, expected %v", tr.Status(), tracer.TrackerStatusStopped)
-	}
-}
 
 type pg struct {
 	id         string
@@ -49,6 +34,22 @@ func (p *pg) Ping(ctx context.Context) error {
 	return nil
 }
 
+func TestRun(t *testing.T) {
+	tr := tracer.NewDefault()
+	if tr.Status() != tracer.TrackerStatusRunning {
+		t.Fatalf("unexpected tracer status: found %v, expected %v", tr.Status(), tracer.TrackerStatusRunning)
+	}
+
+	if err := tr.Run(); err == nil {
+		t.Fatal("tracer should be running")
+	}
+
+	tr.Close()
+	if tr.Status() != tracer.TrackerStatusStopped {
+		t.Fatalf("unexpected tracer status: found %v, expected %v", tr.Status(), tracer.TrackerStatusStopped)
+	}
+}
+
 func TestTrace(t *testing.T) {
 	tr := tracer.NewDefault()
 	p := &pg{shouldFail: false, id: "fake"} // it looks like the host is up
@@ -72,3 +73,4 @@ func TestTrace(t *testing.T) {
 		t.Fatalf("found wrong id: wanted %v, found %v", "fake", id)
 	}
 }
+
