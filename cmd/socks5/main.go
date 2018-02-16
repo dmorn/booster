0e235470d9bda026d3e6992620a45edbd6540547
Daniel Morandini <danielmorandini@me.com>
2018-02-16 15:22:44 +0100

    Improve proxy CLI

--- cmd/socks5/main.go
+++ cmd/socks5/main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"log"
+	"github.com/danielmorandini/booster-network/cmd/socks5/commands"
+)
 
-	"github.com/danielmorandini/booster-network/socks5"
-	"github.com/spf13/cobra"
+// Version and BuildTime are filled in during build by the Makefile
+var (
+	Version   = "N/A"
+	BuildTime = "N/A"
 )
 
 func main() {
-	var pport int
-
-	var cmdStart = &cobra.Command{
-		Use:   "start",
-		Short: "starts a socks5 proxy server",
-		Long:  ``,
-		Args:  cobra.MinimumNArgs(0),
-		Run: func(cmd *cobra.Command, args []string) {
-			p := socks5.SOCKS5()
-			log.Fatal(p.ListenAndServe(pport))
-		},
-	}
-
-	cmdStart.Flags().IntVar(&pport, "pport", 1080, "socks5 listening port")
-
-	var rootCmd = &cobra.Command{Use: "socks5"}
-	rootCmd.AddCommand(cmdStart)
-
-	rootCmd.Execute()
+	commands.Version = Version
+	commands.BuildTime = BuildTime
+	commands.Execute()
 }
+
