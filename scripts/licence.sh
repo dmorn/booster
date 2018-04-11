d32c3302d9e133c98b6a7fabaa04f25011bc7bb9
Daniel Morandini <danielmorandini@me.com>
2018-04-11 23:26:20 +0200

    Improve license script. Add copyright notice

--- scripts/licence.sh
+++ scripts/licence.sh
@@ -1,30 +1,57 @@
-'
-MIT License
+#
+# MIT License
 
-Copyright (c) 2018 Daniel Morandini
+# Copyright (c) 2018 Daniel Morandini
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 
 #!/bin/bash
 
-notice=`cat << EOF
+mit=`cat << EOF
+#
+# MIT License
+
+# Copyright (c) 2018 Daniel Morandini
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+EOF
+`
+
+agpl=`cat << EOF
 /*
 Copyright (C) 2018 Daniel Morandini
 
@@ -43,12 +70,19 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 EOF
 `
+
 tmp=tt.txt
 
 function applyNotice {
 	local target=$1
+	local notice=$2
 	if [ ! -f $target ]; then
 		echo file $target does not exits!
+		return
+	fi
+
+	if [ -z "$notice" ]; then
+		echo no copyright notice provided!
 		return
 	fi
 
@@ -62,12 +96,20 @@ function applyNotice {
 	{ printf "$notice\n\n"; cat $target; } > $tmp && cat $tmp > $target
 }
 
-# find all .go files in the proj
-files=( `find . -name "*.go" -type f -not -path "*vendor*"` )
+# main
 
+# find all .go files in the proj
+files=( `find . -name "*.$name" -type f -not -path "*vendor*"` )
 # apply notice to each file
 for f in "${files[@]}"; do
-	applyNotice $f
+	applyNotice "$f" "$agpl"
+done
+
+# find all .sh files in the proj
+files=( `find . -name "*.sh" -type f ` )
+# apply notice to each file
+for f in "${files[@]}"; do
+	applyNotice "$f" "$mit"
 done
 
 # cleanup
