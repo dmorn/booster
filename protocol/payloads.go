f8ce3cff149ee7e8955113299daa7642c9d96100
Daniel Morandini <danielmorandini@me.com>
2018-05-13 17:13:04 +0200

    Format

--- protocol/payloads.go
+++ protocol/payloads.go
@@ -18,8 +18,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package protocol
 
 import (
-	"fmt"
-	"strings"
 	"time"
 )
 
@@ -31,17 +29,6 @@ type PayloadBandwidth struct {
 	Tot       int    `json:"tot"`
 	Bandwidth int    `json:"bandwidth"`
 	Type      string `json:"type"`
-}
-
-func (n *PayloadBandwidth) String() string {
-	var b strings.Builder
-	b.WriteString("net {\n")
-
-	b.WriteString(fmt.Sprintf("\ttype: %s\n", n.Type))
-	b.WriteString(fmt.Sprintf("\tbandwidth: %v, total: %v\n", n.Bandwidth, n.Tot))
-	b.WriteString("\n}\n")
-
-	return b.String()
 }
 
 type PayloadMonitor struct {
@@ -69,43 +56,12 @@ type PayloadNode struct {
 	Tunnels []*Tunnel `json:"tunnels"`
 }
 
-func (n *PayloadNode) String() string {
-	var b strings.Builder
-	b.WriteString("node {\n")
-
-	id := string([]byte(n.ID)[:10])
-	b.WriteString(fmt.Sprintf("\tid: %v active: %v\n", id, n.Active))
-	b.WriteString(fmt.Sprintf("\tba: %v pa: %v\n", n.BAddr, n.PAddr))
-	b.WriteString("\ttunnels:\n")
-	b.WriteString("\t[")
-	if len(n.Tunnels) == 0 {
-		b.WriteString("]")
-	} else {
-		b.WriteString("\n")
-		for _, t := range n.Tunnels {
-			b.WriteString(fmt.Sprintf("\t\t%v\n", t))
-		}
-		b.WriteString("\t]")
-	}
-	b.WriteString("\n}\n")
-
-	return b.String()
-}
-
 type Tunnel struct {
 	ID        string `json:"id"`
 	Target    string `json:"target"`
 	ProxiedBy string `json:"proxied_by"`
 	Acks      int    `json:"acks"`
 	Copies    int    `json:"copies"`
-}
-
-func (t *Tunnel) String() string {
-	id := string([]byte(t.ID)[:10])
-	return fmt.Sprintf(
-		"{id: %v target: %v proxied_by: %v acks: %v copies %v}",
-		id, t.Target, t.ProxiedBy, t.Acks, t.Copies,
-	)
 }
 
 type PayloadHeartbeat struct {
